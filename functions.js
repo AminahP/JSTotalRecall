700745539af8b36b901914b342ceb1e4216b7a3e
Aminahp <aypitt7@gmail.com>
2022-08-09 15:25:14 -0400

    first commit

---
+++ functions.js
@@ -0,0 +1,27 @@
+// ---- Functions -----
+
+// Print greeting 
+const printGreeting = (name) => {
+    return `Hello there, ${name}!`
+}
+console.log(printGreeting('Slimmer'));
+
+
+// Print cool
+
+const printCool = (name) => {
+    return `${name} is cool`
+}
+console.log(printCool('Captain Reynolds'));
+
+//calculate cube 
+const calculateCube = (num) => {
+    return num * num * num
+}
+console.log(calculateCube(5));
+
+//is vowel
+
+const isVowel = (char) => {
+    if(isVowel === 'a' || isVowel ===)
+}
